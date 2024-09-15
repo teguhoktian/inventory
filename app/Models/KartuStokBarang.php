6a091f2a641df5ddfd475a94bab6500e021122a5
Hoki Teguh Oktian <oktian89@gmail.com>
2024-09-15 18:28:44 +0700

    Membuat Table Kartu Stok

---
+++ app/Models/KartuStokBarang.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class KartuStokBarang extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that aren't mass assignable.
+     *
+     * @var array
+     */
+    protected $guarded = [];
+
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'kartu_stok_barang';
+}
