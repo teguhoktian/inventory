cacfdbcc2ea52bfe6bed9d8b1ec2d120bc08bd39
Hoki Teguh Oktian <oktian89@gmail.com>
2024-09-22 07:14:22 +0700

    Membuat Models untuk Role dan Permission

---
+++ app/Models/Permission.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Spatie\Permission\Models\Permission as ModelsPermission;
+
+class Permission extends ModelsPermission
+{
+    use HasFactory;
+
+    public static function defaultPermissions(): array
+    {
+        return [
+            'view_users',
+            'add_users',
+            'edit_users',
+            'delete_users',
+
+            'view_roles',
+            'add_roles',
+            'edit_roles',
+            'delete_roles',
+        ];
+    }
+}
