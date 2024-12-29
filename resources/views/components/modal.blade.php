7d47af0516ee2276c285db9904e25007ba6afa17
Hoki Teguh Oktian <oktian89@gmail.com>
2024-12-29 12:14:36 +0700

    Update modal component view

--- resources/views/components/modal.blade.php
+++ resources/views/components/modal.blade.php
@@ -1,34 +1,23 @@
 <div class="modal fade" id="{{ $id }}" tabindex="-1" role="dialog" aria-labelledby="{{ $id }}Label" aria-hidden="true">
     <div class="modal-dialog {{ $size ?? '' }}" role="document">
         <div class="modal-content">
-
+            @isset($header)
             <div class="modal-header">
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                    <span aria-hidden="true">×</span>
-                </button>
-                <h4 class="modal-title" id="{{ $id }}Label">{{ $title }}</h4>
+                {{ $header }}
             </div>
-            <!-- /.modal-header -->
+            @endisset
 
+            @isset($body)
             <div class="modal-body">
-                {{ $slot }}
+                {{ $body }}
             </div>
-            <!-- /.modal-body -->
+            @endisset
 
+            @isset($footer)
             <div class="modal-footer">
-                <button type="button" class="btn btn-default pull-left btn-flat" data-dismiss="modal">{{ $closeText ??
-                    __('Close') }}</button>
-                @if (isset($submitText))
-                <button type="submit" class="btn btn-primary btn-flat">{{ $submitText }}</button>
-                @endif
+                {{ $footer }}
             </div>
-            <!-- /.modal-footer -->
-
+            @endisset
         </div>
-        <!-- /.modal-content -->
-
     </div>
-    <!-- /.modal-dialog -->
-
 </div>
-<!-- /.modal -->
