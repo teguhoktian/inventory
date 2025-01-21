18254b9e79c80a6618ae1d14f9afe5f6014db2e9
Hoki Teguh Oktian <oktian89@gmail.com>
2025-01-21 19:58:05 +0700

    Memisahkan bagian Penandatangan menjadi file sendiri

---
+++ resources/views/laporan/signer.blade.php
@@ -0,0 +1,61 @@
+<div style="margin-top: 50px;">
+    @if(count($signer['atasan_dari_atasan']) === 0)
+    <table style="width: 100%; border: none; text-align: center;">
+        <tr>
+            <td style="width: 50%; vertical-align: top;"></td>
+            <td style="width: 50%; vertical-align: top;">
+                {{ __('Cirebon') }}, {{ date('d F Y') }}
+            </td>
+        </tr>
+        <tr>
+            <td style="width: 50%; vertical-align: top;">
+                <p>Mengetahui,</p>
+                <p><strong>{{ $signer['atasan'][0]['position'] }}</strong></p>
+                <br><br><br> <!-- Ruang untuk tanda tangan -->
+                <p>________________________</p>
+                <p><em>{{ $signer['atasan'][0]['name'] }}</em></p>
+            </td>
+            <td style="width: 50%; vertical-align: top;">
+                <p>{{ $signer['user']['position'] }}</p>
+                <p><strong>{{ __('Dibuat oleh') }},</strong></p>
+                <br><br><br> <!-- Ruang untuk tanda tangan -->
+                <p>________________________</p>
+                <p><em>{{ $signer['user']['name'] }}</em></p>
+            </td>
+        </tr>
+    </table>
+    @else
+    <table style="width: 100%; border: none; text-align: center;">
+        <tr>
+            <td style="width: 33%; vertical-align: top;"></td>
+            <td style="width: 33%; vertical-align: top;"></td>
+            <td style="width: 33%; vertical-align: top;">
+                {{ __('Cirebon') }}, {{ date('d F Y') }}
+            </td>
+        </tr>
+        <tr>
+            <td style="width: 33%; vertical-align: top;">
+                <p>Mengetahui,</p>
+                <p><strong>{{ $signer['atasan_dari_atasan'][0]['position'] }}</strong></p>
+                <br><br><br> <!-- Ruang untuk tanda tangan -->
+                <p>________________________</p>
+                <p><em>{{ $signer['atasan_dari_atasan'][0]['name'] }}</em></p>
+            </td>
+            <td style="width: 33%; vertical-align: top;">
+                <p>Diperiksa oleh,</p>
+                <p><strong>{{ $signer['atasan'][0]['position'] }}</strong></p>
+                <br><br><br> <!-- Ruang untuk tanda tangan -->
+                <p>________________________</p>
+                <p><em>{{ $signer['atasan'][0]['name'] }}</em></p>
+            </td>
+            <td style="width: 33%; vertical-align: top;">
+                <p>{{ $signer['user']['position'] }}</p>
+                <p><strong>{{ __('Dibuat oleh') }},</strong></p>
+                <br><br><br> <!-- Ruang untuk tanda tangan -->
+                <p>________________________</p>
+                <p><em>{{ $signer['user']['name'] }}</em></p>
+            </td>
+        </tr>
+    </table>
+    @endif
+</div>
